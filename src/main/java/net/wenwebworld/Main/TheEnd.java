5cdce43f40b4aba7cc93469895f0cc0537a7ef0f
BuildTools <unconfigured@null.spigotmc.org>
2022-08-16 10:24:24 +0800

    Initial : Empty Project.

---
+++ src/main/java/net/wenwebworld/Main/TheEnd.java
@@ -0,0 +1,17 @@
+package net.wenwebworld.Main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TheEnd extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
